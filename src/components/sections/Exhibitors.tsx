133ae3b9ceb9dd7d1eeb4e0f432173a96fad930c
livetolead23-cpu <livetolead23@gmail.com>
2026-07-29 09:53:35 -0400

    Reorder exhibitors

--- src/components/sections/Exhibitors.tsx
+++ src/components/sections/Exhibitors.tsx
@@ -9,9 +9,9 @@ const EXHIBITORS = [
   { name: "Keller Williams",    logo: "/images/exhibitors/kellerwilliams.png",    w: 160, h: 72 },
   { name: "ScotiaBank",         logo: "/images/exhibitors/scotiabank.png",         w: 160, h: 72 },
   { name: "Colina",             logo: "/images/exhibitors/colina.png",             w: 160, h: 72 },
-  { name: "Bold Pineapple",     logo: "/images/exhibitors/boldpineapple.png",      w: 188, h: 92 },
-  { name: "Registrar General",  logo: "/images/exhibitors/registrargeneral.png",   w: 160, h: 72 },
   { name: "SBDC Bahamas",       logo: "/images/exhibitors/SBDC.png",               w: 160, h: 72 },
+  { name: "Registrar General",  logo: "/images/exhibitors/registrargeneral.png",   w: 160, h: 72 },
+  { name: "Bold Pineapple",     logo: "/images/exhibitors/boldpineapple.png",      w: 188, h: 92 },
 ];
 
 export default function Exhibitors() {
